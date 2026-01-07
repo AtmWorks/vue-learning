74ea89ab965245085d71f746c0ab61496aa2f938
AtmWorks <atmgameworks@gmail.com>
2026-01-07 20:39:46 +0100

    Creating second page from mockup

---
+++ src/Lesson03.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from "@/components/Lesson03/Header.vue";
+import Main from "@/components/Lesson03/Main.vue";
+import Footer from "@/components/Lesson03/Footer.vue";
+</script>
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
+<style scoped></style>
